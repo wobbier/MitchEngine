9c5c3268aff5979bb3be40989e6713aa4bd72e40
Mitch Andrews <rastaninja77@gmail.com>
2019-01-27 02:11:26 -0500

    📦 Possible nuget build server fix

--- ThirdParty/GenerateSolutions.bat
+++ ThirdParty/GenerateSolutions.bat
@@ -1,7 +1,5 @@
 cd Assimp
-cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0.14393.0
+cmake -G "Visual Studio 15 2017 Win64" -DBUILD_SHARED_LIBS=false
 cd ../Bullet
 cmake -G "Visual Studio 15 2017 Win64" -DUSE_MSVC_RUNTIME_LIBRARY_DLL=true
-cd ../Brofiler
-call GenerateProjects.bat
 cd ../
