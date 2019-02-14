78748477208f71c2510f3f39aa0c307c692b1827
Mitch Andrews <rastaninja77@gmail.com>
2019-02-13 23:52:28 -0500

    🛠 Potential build fix

--- ThirdParty/GenerateSolutions.bat
+++ ThirdParty/GenerateSolutions.bat
@@ -1,5 +1,5 @@
 cd Assimp
-cmake -G "Visual Studio 15 2017 Win64" -DBUILD_SHARED_LIBS=false
+cmake . -G "Visual Studio 15 2017 Win64" -DBUILD_SHARED_LIBS=false
 cd ../Bullet
-cmake -G "Visual Studio 15 2017 Win64" -DUSE_MSVC_RUNTIME_LIBRARY_DLL=true
+cmake . -G "Visual Studio 15 2017 Win64" -DUSE_MSVC_RUNTIME_LIBRARY_DLL=true
 cd ../
