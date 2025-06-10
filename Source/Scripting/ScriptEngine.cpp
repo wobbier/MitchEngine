f4610098e940d19c8b95aa6ce5fe751253be31a1
Mitch Andrews <rastaninja77@gmail.com>
2025-06-10 04:56:33 -0400

    🖼️ Logging better times

--- Source/Scripting/ScriptEngine.cpp
+++ Source/Scripting/ScriptEngine.cpp
@@ -23,6 +23,7 @@
 
 #include "Bindings/ImGui.bindings.h"
 #include "Bindings/World.bindings.h"
+#include "Bindings/Log.bindings.h"
 
 //// forward declare internal Mono debugger agent functions
 //extern "C" {
@@ -72,45 +73,6 @@ std::unordered_map<EntityID, uint32_t> ScriptEngine::entityInstanceCache;
 //    ResendDebugInfo( ScriptEngine::sScriptData.AppAssembly );
 //}
 
-static void Log( MonoString* inString )
-{
-    char* str = mono_string_to_utf8( inString );
-
-    BRUH( str );
-
-    mono_free( str );
-}
-
-static void LogError( MonoString* inString )
-{
-    char* str = mono_string_to_utf8( inString );
-
-    YIKES( str );
-
-    mono_free( str );
-}
-
-static void NativeLog( MonoString* inString, int number )
-{
-    char* str = mono_string_to_utf8( inString );
-
-    std::cout << str << std::to_string( number ) << std::endl;
-
-    mono_free( str );
-}
-
-static void NativeLog_Vector( Vector3* inVector, Vector3* outVec )
-{
-    Vector3 vec = *inVector;
-    std::cout << "CPP: " << std::to_string( vec.x ) << ", " << std::to_string( vec.y ) << ", " << std::to_string( vec.z ) << std::endl;
-    inVector->Normalize();
-    *outVec = *inVector;
-}
-
-static float Native_VectorLength( Vector3* inVector )
-{
-    return inVector->Length();
-}
 
 
 MonoObject* ScriptEngine::ReturnEntityID( const EntityHandle& inEntity )
@@ -285,13 +247,9 @@ void ScriptEngine::InitMono()
 
 void ScriptEngine::RegisterFunctions()
 {
-    mono_add_internal_call( "Debug::Log", (void*)Log );
-    mono_add_internal_call( "Debug::Error", (void*)LogError );
-    mono_add_internal_call( "TestScript::NativeLog", (void*)NativeLog );
-    mono_add_internal_call( "TestScript::NativeLog_Vector", (void*)NativeLog_Vector );
-    mono_add_internal_call( "TestScript::Native_VectorLength", (void*)Native_VectorLength );
     Register_ImGuiBindings();
     Register_WorldBindings();
+    Register_LogBindings();
 }
 
 
